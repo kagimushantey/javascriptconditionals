3fa62c5bea95de23658857ae5cb031e45314bd92
kagimushantey <kagimushantey@gmail.com>
2022-10-21 11:01:57 +0300

    first commit

---
+++ login.js
@@ -0,0 +1,50 @@
+ let names= ["sarah","sharon","shaluwat","sam","steve"];/* array declaration*/
+ /*start of for loop*/
+//  for(let i = 0; i < names.length; i++ ){
+//      if(names[i] =="shaluwat"){
+//           console.log( names[i] + ":this is my name");
+//           continue;
+//      }
+//      console.log(names[i]);
+//  }
+ /*end of for loop*/
+
+
+ /*start of for each loop*/
+//  names.forEach(function(names){
+//      if(names == "shaluwat"){
+//           console.log(  "shaluwat:this is my name"); 
+//      }
+// console.log(names);
+//  });
+  /*end of for each loop*/
+
+
+
+  /*start of do while loop*/
+//   let i = 0;
+//   do{
+//      if(names[i] == "shaluwat"){
+//      names[2]=(names[2] +" "+ "this is my name");
+//      }
+//      console.log(names[i]);
+//      i++;
+//   } while( i < names.length);
+  
+
+   /*end of do while loop*/
+   /*start of for of loop*/
+//    for( const element of names){
+//           if(element =="shaluwat"){
+//                console.log( element + ":this is my name");
+//                continue;
+//           }
+          
+   
+//                console.log(element);
+//              }
+     
+   
+   /* end of for of loop*/
+
+
